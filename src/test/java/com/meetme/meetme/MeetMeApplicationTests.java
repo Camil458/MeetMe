9ad835f52fad08e43362b93efff63f964328bd26
Camil458 <k.radlowski458@gmail.com>
2023-03-30 01:52:23 +0200

    Save new user test

--- src/test/java/com/meetme/meetme/MeetMeApplicationTests.java
+++ src/test/java/com/meetme/meetme/MeetMeApplicationTests.java
@@ -2,8 +2,9 @@ package com.meetme.meetme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(classes = MeetMeApplication.class)
 class MeetMeApplicationTests {
 
     @Test
