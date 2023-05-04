6ce6b4f917b5f3d3b0b4dc6a1acafb3385fe91fc
WSokoll <wladek.sokol01@gmail.com>
2023-05-04 21:18:09 +0200

    Bug fix - EventController

--- src/main/java/com/meetme/meetme/controller/EventController.java
+++ src/main/java/com/meetme/meetme/controller/EventController.java
@@ -3,19 +3,17 @@ package com.meetme.meetme.controller;
 import com.meetme.meetme.service.GoogleSearchService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin
 @RequestMapping(path = "/events")
 public class EventController {
     GoogleSearchService googleSearchService;
 
     @GetMapping
-    public ResponseEntity<String> getEvents(@RequestParam(value = "g") String q, @RequestParam(value = "date", required = false) String date) {
+    public ResponseEntity<String> getEvents(@RequestParam(value = "q") String q, @RequestParam(value = "date", required = false) String date) {
         if (date != null) {
             return ResponseEntity.ok(googleSearchService.search(q, date));
         } else {
