ef578052db037f2da389e3bec5195e13102b1285
Camil458 <k.radlowski458@gmail.com>
2023-05-10 20:19:19 +0200

    Redirect to frontend main page

--- src/main/java/com/meetme/meetme/controller/RegistrationController.java
+++ src/main/java/com/meetme/meetme/controller/RegistrationController.java
@@ -2,16 +2,24 @@ package com.meetme.meetme.controller;
 
 import com.meetme.meetme.model.UserDTO;
 import com.meetme.meetme.service.RegistrationService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/registration")
 public class RegistrationController {
+
+    @Value("${frontend_url}")
+    private String FRONTEND_URL;
 
     private final RegistrationService registrationService;
 
@@ -23,9 +31,14 @@ public class RegistrationController {
     }
 
     @GetMapping(path = "confirm")
-    public ResponseEntity<String> confirm(@RequestParam("token") String token){
+    public ResponseEntity<Object> confirm(@RequestParam("token") String token) throws URISyntaxException {
         registrationService.confirmToken(token);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        // redirect to frontend main page
+        URI uri = new URI(FRONTEND_URL);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uri);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.valueOf(302));
     }
 }
--- src/main/java/com/meetme/meetme/mapper/EventMapper.java
+++ src/main/java/com/meetme/meetme/mapper/EventMapper.java
@@ -3,7 +3,6 @@ package com.meetme.meetme.mapper;
 import com.meetme.meetme.entity.Event;
 import com.meetme.meetme.model.EventDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper
 public interface EventMapper {
